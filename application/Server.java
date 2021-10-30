4d58db9957124e5f005530650e457fc7cda0de74
kieranmcgregor <kieranmcgregor@github.com>
2021-10-30 10:56:43 -0400

    Changed index.html

--- application/Server.java
+++ application/Server.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 
 public class Server
 {
-	public static final String PROJECT_PATH = "/var/www/project/";
+	public static final String PROJECT_PATH = "/var/www/COMP3005Project/";
 
     public static void main(String[] args) throws Exception
     {
